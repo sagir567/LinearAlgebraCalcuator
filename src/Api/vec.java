d829807b64b94fb0f3eda40ced9112bc818ed0e1
sagir567 <sagir567@gmail.com>
2022-10-20 05:22:24 +0300

    vec: deleted two methods ("multiply",getVecArr)

--- src/Api/vec.java
+++ src/Api/vec.java
@@ -1,11 +1,13 @@
 package Api;
 
+import java.sql.Array;
+
 public interface vec<T> {
 
 
 
 
-    public void sum (vec v); // return the sum of the different vectors
+    public void sum (vec v) throws Exception; // return the sum of the different vectors
 //    public void multiply(T n); // multiply the current Departments.naturalVec with the given input
     public int getDim();// return the dimension of the vector
 //    public Array[] getVecArr();// return an array who represents the Api.vec
